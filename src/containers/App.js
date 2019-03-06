95fc13da8a764fdc6a58b97c35675e493a951e8d
Daniel <daniel@balancedsolutions.se>
2019-03-06 17:38:28 +0100

    Small update

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList'
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 
@@ -41,7 +42,9 @@ class App extends Component {
                  <SearchBox searchChange={this.onSearchChange} />
                 
             <Scroll>  
-                <CardList robots={filteredRobots}/>
+                <ErrorBoundry>
+                    <CardList robots={filteredRobots}/>
+                </ErrorBoundry>
             </Scroll> 
             </div>
              );
